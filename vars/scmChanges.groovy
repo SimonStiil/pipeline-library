57f05606a0f78e8dad5781a3fdef70ec64faf922
simon <25962382+burads@users.noreply.github.com>
2021-07-07 13:58:13 +0200

    Adding fixing regex
    removing debugging

--- vars/scmChanges.groovy
+++ vars/scmChanges.groovy
@@ -1,19 +1,19 @@
 #!/usr/bin/env groovy
 @NonCPS
 boolean call(String value) {
-	def testValue = "/^"+value+"/"
-	for (changeLogSet in currentBuild.changeSets) { 
+	def testValue = "^"+value
+	if (testValue.contains("/"))
+		testValue = testValue.replace('/','\\/')
+	if (testValue.endsWith("/"))
+		testValue += '.*'
+	for (changeLogSet in currentBuild.changeSets) {
 		for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
 			for (file in entry.getAffectedFiles()) {
 				if (file.getPath() ==~ testValue) {
-					printf("+ %s ==~ %s",file.getPath(),testValue)
 					return true
 				}
-				else
-					printf("- %s ==~ %s",file.getPath(),testValue)
 			}
 		}
 	}
-	print("scmChanges false")
 	return false
 }
