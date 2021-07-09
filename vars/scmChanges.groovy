0c39aa983470d6dfe828c1048aa227f27e7589e2
simon <25962382+burads@users.noreply.github.com>
2021-07-09 12:53:42 +0200

    moving to use startsWith rather then regex

--- vars/scmChanges.groovy
+++ vars/scmChanges.groovy
@@ -1,15 +1,10 @@
 #!/usr/bin/env groovy
 @NonCPS
 boolean call(String value) {
-	def testValue = "^"+value
-	if (testValue.contains("/"))
-		testValue = testValue.replace('/','\\/')
-	if (testValue.endsWith("/"))
-		testValue += '.*'
 	for (changeLogSet in currentBuild.changeSets) {
 		for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
 			for (file in entry.getAffectedFiles()) {
-				if (file.getPath() ==~ testValue) {
+				if (file.getPath().startsWith(value)) {
 					return true
 				}
 			}
