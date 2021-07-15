1b053f90a72ed8a9e5d916ebf10c86be6a42340b
w29744 <25962382+burads@users.noreply.github.com>
2021-07-15 14:57:19 +0200

    adding isReleseTag() function to test for release tag

---
+++ src/dk/stiil/pipeline/Constants.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+package dk.stiil.pipeline
+
+class Constants {
+    final static Pattern RELEASE_PATTERN = Pattern.compile("release\\/\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?")
+    final static Pattern TAG_RELEASE_PATTERN = Pattern.compile("^v\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?\$")
+    final static Pattern TAG_RELEASE_PATTERN = Pattern.compile("^v\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?\$")
+    final static Pattern TAG_RELEASE_CANDIDATE_PATTERN = Pattern.compile("^\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?(-rc-\\d{1,2})?")
+    final static Pattern PULL_REQUEST_PATTERN = Pattern.compile("^PR-\\d{1,5}")
+    final static Pattern FEATURE_PATTERN = Pattern.compile("^feature\\/.+")
+    final static Pattern BUGFIX_PATTERN = Pattern.compile("^bugfix\\/.+/")
+    final static Pattern MAIN_PATTERN = Pattern.compile("^main")
+}
---
+++ vars/isReleaseTag.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+import static dk.ufst.devops.branchnaming.Constants.*
+
+def call() {
+	def matcher = BRANCH_NAME =~ TAG_RELEASE_PATTERN
+	return matcher.find())
+}
