82f03c16d7a51c3ef7e4db1eda990281ac8cfea8
simon <25962382+burads@users.noreply.github.com>
2021-07-15 17:26:49 +0200

    trying not to be stupid, copy paste

--- vars/isReleaseTag.groovy
+++ vars/isReleaseTag.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-import static dk.ufst.devops.branchnaming.Constants.*
+import static dk.stiil.pipeline.Constants.*
 
 def call() {
 	def matcher = BRANCH_NAME =~ TAG_RELEASE_PATTERN
