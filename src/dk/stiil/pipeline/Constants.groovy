474cd7901d85d773563242eda01f9fa1d9176920
simon <25962382+burads@users.noreply.github.com>
2021-07-15 17:29:47 +0200

    removing duplicate line

--- src/dk/stiil/pipeline/Constants.groovy
+++ src/dk/stiil/pipeline/Constants.groovy
@@ -6,7 +6,6 @@ import java.util.regex.Pattern
 class Constants {
     final static Pattern RELEASE_PATTERN = Pattern.compile("release\\/\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?")
     final static Pattern TAG_RELEASE_PATTERN = Pattern.compile("^v\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?\$")
-    final static Pattern TAG_RELEASE_PATTERN = Pattern.compile("^v\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?\$")
     final static Pattern TAG_RELEASE_CANDIDATE_PATTERN = Pattern.compile("^\\d{1,3}\\.\\d{1,3}(\\.\\d{1,3})?(-rc-\\d{1,2})?")
     final static Pattern PULL_REQUEST_PATTERN = Pattern.compile("^PR-\\d{1,5}")
     final static Pattern FEATURE_PATTERN = Pattern.compile("^feature\\/.+")
