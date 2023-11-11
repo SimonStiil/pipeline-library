571279360adb4d9b52f3e718dd42c2b42eeb7bbe
simon <130985998+SimonStiil@users.noreply.github.com>
2023-11-11 19:20:00 +0100

    switch to using branch from tag

--- vars/githubPackageCleanupVersions.groovy
+++ vars/githubPackageCleanupVersions.groovy
@@ -1,5 +1,14 @@
 #!/usr/bin/env groovy
 def call(Map data) {
+/* Required:
+ *   github_event
+ *   repository
+ *   repository_short
+ * Optional :
+ *   branch
+ *   credentialId
+ *   organization
+ */
     if (!data.credentialId){
         data.credentialId = "github-login-secret"
     }
@@ -17,9 +26,9 @@ def call(Map data) {
             }
 
         } else {
-            if (data.tag && data.github_event == "delete") {
+            if (data.branch && data.github_event == "delete") {
                 for (tag in item['metadata']['container']['tags']) {
-                    if (tag == data.tag){
+                    if (tag == data.branch){
                         Map config = new LinkedHashMap<String,String>(data)
                         config.version = item['id']
                         if (githubPackageDeleteVersion(config)){
