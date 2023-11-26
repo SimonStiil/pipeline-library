d56cbd56aab6901e90bb4986b0a798b55b3efa33
simon <130985998+SimonStiil@users.noreply.github.com>
2023-11-26 10:53:27 +0100

    LAB-3 doing a bit of cleanup for packages

--- vars/githubPackageDeleteVersion.groovy
+++ vars/githubPackageDeleteVersion.groovy
@@ -1,14 +1,26 @@
 #!/usr/bin/env groovy
 boolean call(Map properties) {
+    if (!properties.credentialId){
+        properties.credentialId = "github-login-secret"
+    }
+    if (!properties.debug){
+        properties.debug = false
+    }
+    if (!properties.quiet){
+        properties.quiet = true
+    }
     // https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#delete-package-version-for-a-user
     withCredentials([usernamePassword(credentialsId: properties.credentialId,
                                       usernameVariable : 'GITHUB_USERNAME',
                                       passwordVariable: 'GITHUB_PERSONAL' )]) {
         def response = httpRequest customHeaders: [[name: 'Accept', value: 'application/vnd.github+json'],
-                                                   [name: 'Authorization', value: "Bearer $GITHUB_PERSONAL"],
+                                                   [name: 'Authorization', value: 'Bearer ' + GITHUB_PERSONAL],
                                                    [name: 'X-GitHub-Api-Version', value: '2022-11-28']],
                 httpMode: 'DELETE',
-                url: "https://api.github.com/users/${properties.organization}/packages/${properties.PACKAGE_TYPE}/${properties.PACKAGE_NAME}/versions/${properties.version}"
+                url: "https://api.github.com/users/${properties.organization}/packages/${properties.PACKAGE_TYPE}/${properties.PACKAGE_NAME}/versions/${properties.version}",
+                consoleLogResponseBody: properties.debug,
+                quiet: properties.quiet,
+                wrapAsMultipart: false
         if (response.status != 204) {
             echo "Failed to delete ${properties.PACKAGE_TYPE} ${properties.organization}/${properties.PACKAGE_NAME} version ${properties.version}"
             return false
--- vars/githubPackagesListVersions.groovy
+++ vars/githubPackagesListVersions.groovy
@@ -1,13 +1,25 @@
 #!/usr/bin/env groovy
 def call(Map properties) {
+    if (!properties.credentialId){
+        properties.credentialId = "github-login-secret"
+    }
+    if (!properties.debug){
+        properties.debug = false
+    }
+    if (!properties.quiet){
+        properties.quiet = true
+    }
     // https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#list-package-versions-for-a-package-owned-by-a-user
     withCredentials([usernamePassword(credentialsId: properties.credentialId,
             usernameVariable : 'GITHUB_USERNAME',
             passwordVariable: 'GITHUB_PERSONAL' )]) {
         def response = httpRequest customHeaders: [[name: 'Accept', value: 'application/vnd.github+json'],
-                                                   [name: 'Authorization', value: "Bearer $GITHUB_PERSONAL"],
+                                                   [name: 'Authorization', value: 'Bearer ' + GITHUB_PERSONAL],
                                                    [name: 'X-GitHub-Api-Version', value: '2022-11-28']],
-                url: "https://api.github.com/users/${properties.organization}/packages/${properties.PACKAGE_TYPE}/${properties.PACKAGE_NAME}/versions"
+                url: "https://api.github.com/users/${properties.organization}/packages/${properties.PACKAGE_TYPE}/${properties.PACKAGE_NAME}/versions",
+                consoleLogResponseBody: properties.debug,
+                quiet: properties.quiet,
+                wrapAsMultipart: false
         def jsonResponse = readJSON text: response.content
         return jsonResponse
     }
