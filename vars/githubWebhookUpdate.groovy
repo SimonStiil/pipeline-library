d570a60d83c68c3d75ab552976c46b082c250896
simon <130985998+SimonStiil@users.noreply.github.com>
2023-11-26 08:57:43 +0100

    LAB-3 working on update fix json

--- vars/githubWebhookUpdate.groovy
+++ vars/githubWebhookUpdate.groovy
@@ -17,14 +17,15 @@ Optional:
                 usernameVariable: 'GITHUB_USERNAME',
                 passwordVariable: 'GITHUB_PERSONAL')]) {
             // curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_PERSONAL" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/SimonStiil/keyvaluedatabase/hooks/442580848 -X PATCH -d '{"events":["delete", "push"]}'
-            def body = '{"events":'+data.events.toString()+'}'
+            String jsonEvents = writeJSON returnText: true, json: data.events
+            String body = '{"events":'+jsonEvents+'}'
             echo "githubWebhookUpdate (body): " + body
             httpRequest customHeaders: [[name: 'Accept', value: 'application/vnd.github+json'],
                                                        [name: 'Authorization', value: 'Bearer ' + GITHUB_PERSONAL],
                                                        [name: 'X-GitHub-Api-Version', value: '2022-11-28']],
                     httpMode: 'PATCH',
                     url: "https://api.github.com/repos/${data.gitMap.fullName}/hooks/${data.hookId}",
-                    requestBody: '{"events":'+data.events.toString()+'}'
+                    requestBody: body
         }
     }
     if ( data.url ) {
