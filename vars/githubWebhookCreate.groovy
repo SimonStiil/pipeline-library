b151831c83436aa035e9f57d585e8ad711c6c589
simon <130985998+SimonStiil@users.noreply.github.com>
2023-11-26 09:36:02 +0100

    LAB-3 working on create - fix events

--- vars/githubWebhookCreate.groovy
+++ vars/githubWebhookCreate.groovy
@@ -22,12 +22,13 @@ Optional:
     withCredentials([usernamePassword(credentialsId: data.credentialId,
             usernameVariable: 'GITHUB_USERNAME',
             passwordVariable: 'GITHUB_PERSONAL')]) {
+        String jsonEvents = writeJSON returnText: true, json: data.events
         httpRequest customHeaders: [[name: 'Accept', value: 'application/vnd.github+json'],
                                     [name: 'Authorization', value: 'Bearer ' + GITHUB_PERSONAL],
                                     [name: 'X-GitHub-Api-Version', value: '2022-11-28']],
                 httpMode: 'POST',
                 url: "https://api.github.com/repos/${data.gitMap.fullName}/hooks",
-                requestBody: '{"name":"web","active":true,"events":'+data.events.toString()+','+
+                requestBody: '{"name":"web","active":true,"events":'+jsonEvents+','+
                         '"config":{"url":"'+ data.url +'","content_type":"json","insecure_ssl":"0"}}',
                 consoleLogResponseBody: data.debug,
                 quiet: data.quiet,
