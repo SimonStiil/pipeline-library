d3e9eb8d4050cc626e30a09925bd44f9df751652
Simon Stiil <130985998+SimonStiil@users.noreply.github.com>
2025-11-08 11:54:00 +0100

    adding get version

---
+++ vars/githubPackageGetVersion.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+def call(Map properties) {
+    if (!properties.credentialId){
+        properties.credentialId = "github-login-secret"
+    }
+    if (!properties.debug){
+        properties.debug = false
+    }
+    if (!properties.quiet){
+        properties.quiet = true
+    }
+    // https://docs.github.com/en/rest/packages/packages?apiVersion=2022-11-28#get-a-package-version-for-a-user
+    withCredentials([usernamePassword(credentialsId: properties.credentialId,
+            usernameVariable : 'GITHUB_USERNAME',
+            passwordVariable: 'GITHUB_PERSONAL' )]) {
+        def response = httpRequest customHeaders: [[name: 'Accept', value: 'application/vnd.github+json'],
+                                                   [name: 'Authorization', value: 'Bearer ' + GITHUB_PERSONAL],
+                                                   [name: 'X-GitHub-Api-Version', value: '2022-11-28']],
+                url: "https://api.github.com/users/${properties.organization}/packages/${properties.PACKAGE_TYPE}/${properties.PACKAGE_NAME}/versions/${properties.version}",
+                consoleLogResponseBody: properties.debug,
+                quiet: properties.quiet,
+                wrapAsMultipart: false
+        def jsonResponse = readJSON text: response.content
+        return jsonResponse
+    }
+}
